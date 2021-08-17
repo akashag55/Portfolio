9542d4ae8f5e81dce7db395cdfc421be7cf51bd3
akashag55 <2017pietcsakash005@poornima.org>
2021-08-17 15:29:56 +0530

    UI changes and nav bar changes

--- src/Components/GetInTouch/GetInTouch.jsx
+++ src/Components/GetInTouch/GetInTouch.jsx
@@ -7,10 +7,13 @@ function GetInTouch() {
         <div>
             <SectionHeading heading_name = "Get In Touch"/>
                <div className = "touch_data">
-                    <h2>Mail - <span>agarwalakash551999@gmail.com</span></h2>
-                    <h2>Text - <span>8890587002</span></h2>
+                    <h2>Mail - <span><a href='mailto:agarwalakash551999@gmail.com'>agarwalakash551999@gmail.com</a></span></h2>
+                    <h2>Call - <span>8890587002</span></h2>
                     <h2><a href="https://www.linkedin.com/in/akash-agarwal-2a2297150">Linkedin</a> & <a href="https://github.com/akashag55">GitHub</a>, <a href="https://drive.google.com/file/d/1lnJM6EeAoRgFBSyIcQhUvxqjAldVRnL1/view">Download Resume</a></h2>
+
                </div>
+
+
         </div>
     )
 }
