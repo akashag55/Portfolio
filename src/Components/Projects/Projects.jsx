51606d73a72eac120ff6f7e7b192eb4b5ed05a5b
akashag55 <2017pietcsakash005@poornima.org>
2021-09-02 01:27:47 +0530

    Section heading and project animation added

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SectionHeading from '../SestionHeading/SectionHeading'
+import project_img from '../../Img/project_img.jpg'
 import './Projects.css'
 
 function Projects() {
@@ -8,24 +9,24 @@ function Projects() {
         {
             id:1,
             name: 'CRWN CLOATHING',
-            description: 'This is a E-commerce website with all fashionable products built with React.',
+            description: 'This is a E-commerce website built with React.',
             link: 'https://crwn-live.herokuapp.com/'
         },
         {
             id:2,
             name: 'EXPANCE TRACKER',
-            description: 'This is a dynamic Expense Tracker with chart functionality with audio command to insert transaction and transaction history built with React Hooks and Spechly.',
+            description: 'This is a dynamic Expense Tracker with chart functionality.',
             link: 'https://expense-tracker-2.netlify.app/'
         },
         {
-            id:1,
+            id:3,
             name: 'KEEPER APP',
-            description: 'This is a clone of Google Keep.',
+            description: 'This is a clone of Google Keep to keep your notes updated.',
             link: 'https://keeper-app-65239.web.app/'
         },
         {
-            id:1,
-            name: 'REL TIME CHAT APP (WHATSAPP CLONE)',
+            id:4,
+            name: 'REL TIME CHAT APP',
             description: 'This is a Chat Applications for browsers with local storage.',
             link: 'https://whatsapp-clone-chatapp.netlify.app/'
         }
@@ -38,12 +39,20 @@ function Projects() {
             <div className="project_section_card">
                 {
                     project_list.map(project => 
-                        <div className="project_section_card_data">
-                        <a href={project.link}>
+                        <>
+                        <div className={`project_section_card_data project_section_card_animation_${project.id}`}>
                             <p className="project_section_card_name">{project.name}</p>
+                            <img src={project_img} alt='project_image'/>
                             <p className="project_section_card_description">{project.description}</p>
-                        </a>
+                        {
+                            (
+                            <div className="project_section_card_buttons">
+                                <button>Source</button>
+                                <a href={project.link}><button>Demo</button></a>
+                            </div>
+                            )}
                         </div>
+                        </>
                     )
                 }
             </div>
